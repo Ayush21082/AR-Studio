3097c0e2741814d8dd1285dd55913ad85580c789
Ayush Singh <ayush.singh21082@gmail.com>
2021-08-29 21:14:41 +0530

    Body Tracking Logics

---
+++ Body Tracking/Body Tracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Body Tracking
+//
+//  Created by Ayush Singh on 29/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
